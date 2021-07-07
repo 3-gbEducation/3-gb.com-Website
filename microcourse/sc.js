c9b2d5487053f61d0c94f68c95abd19ed6596f2f
Kanika Mahal <kanikamahal123@gmail.com>
2021-07-07 16:33:16 +0530

    microcourse

--- microcourse/sc.js
+++ microcourse/sc.js
@@ -19,7 +19,7 @@ firstNextBtn.addEventListener("click",function(){
     slidePage.style.marginLeft = "-25%";
     bullet[current -1].classList.add("active");
     progrssTxt[current -1].classList.add("active");
-    progrssCheck[current -1].classList.add("active");
+    
     current += 1;
 
 });
@@ -27,53 +27,54 @@ nextBtnSec.addEventListener("click",function(){
     slidePage.style.marginLeft = "-50%";
     bullet[current -1].classList.add("active");
     progrssTxt[current -1].classList.add("active");
-    progrssCheck[current -1].classList.add("active");
+    
     current += 1;
 
 });
 nextBtnThird.addEventListener("click",function(){
     slidePage.style.marginLeft = "-75%";
     bullet[current -1].classList.add("active");
-    progrssTxt[current -1].classList.add("active");
-    progrssCheck[current -1].classList.add("active");
+    progrssTxt[current - 1].classList.add("active");
+    
     current += 1;
 
 });
-submitBtn.addEventListener("click",function(){
+
+submitBtn.addEventListener("click", function(){
+    bullet[current - 1].classList.add("active");
     
-    bullet[current -1].classList.add("active");
-    progrssTxt[current -1].classList.add("active");
-    progrssCheck[current -1].classList.add("active");
+    progrssTxt[current - 1].classList.add("active");
     current += 1;
     setTimeout(function(){
-        alert("Thanks");
-        location.reload();
-
+      alert("Your Form Successfully Signed up");
+      location.reload();
     },800);
+  });
+  
 
-});
 
 
-prevBtnSec.addEventListener("click",function(){
-    slidePage.style.marginLeft = "0%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
 prevBtnThird.addEventListener("click",function(){
     slidePage.style.marginLeft = "-25%";
     
     bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
+    
+    progrssTxt[current - 2].classList.remove("active");
     current -= 1;
 });
 prevBtnFourth.addEventListener("click",function(){
     slidePage.style.marginLeft = "-50%";
     
     bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
+    
+    progrssTxt[current - 2].classList.remove("active");
     current -= 1;
 });
+prevBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    
+    progrssTxt[current - 2].classList.remove("active");
+    current -= 1;
+  });
