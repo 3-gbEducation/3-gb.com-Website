0a122ad645c4505dbbbd37ce540ba004f405b1bf
warscorpio <71835907+warscorpio@users.noreply.github.com>
2021-07-16 20:39:59 +0530

    Add files via upload
    login and signup page connected to database(mongodb)

---
+++ model/user.js
@@ -0,0 +1,10 @@
+
+var mongoose=require("mongoose");
+var passportlocalmongoose=require("passport-local-mongoose");
+var UserSchema=mongoose.Schema({
+username: String,
+Password: String
+});
+
+UserSchema.plugin(passportlocalmongoose);
+module.exports=mongoose.model("User", UserSchema);
